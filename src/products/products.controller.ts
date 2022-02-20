1b71bfe9640352c90917eb5735fe46c14f094407
s-cryptor <grisga2001@gmail.com>
2022-02-20 20:57:41 +0300

    update products service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { Product } from 'src/schemas/product.schema'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
 import { ProductsService } from './products.service'
@@ -8,27 +9,30 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  getAll() {
+  getAll(): Promise<Product[]> {
     return this.productsService.getAll()
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): string {
+  getOne(@Param('id') id: string): Promise<Product> {
     return this.productsService.getById(id)
   }
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productsService.create(createProductDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return 'Remove ' + id
+  remove(@Param('id') id: string): Promise<Product> {
+    return this.productsService.remove(id)
   }
 
   @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
-    return 'Update ' + id
+  update(
+    @Body() updateProductDto: UpdateProductDto,
+    @Param('id') id: string
+  ): Promise<Product> {
+    return this.productsService.update(id, updateProductDto)
   }
 }
