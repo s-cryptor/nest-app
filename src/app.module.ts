016de1432552ddf68c908803cb82ebe70f3f651c
s-cryptor <grisga2001@gmail.com>
2022-02-20 20:44:23 +0300

    add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ProductsModule } from './products/products.module'
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      `mongodb://127.0.0.1:27017/products?directConnection=true&serverSelectionTimeoutMS=2000`
+    )
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Product, ProductSchema } from 'src/schemas/product.schema'
 import { ProductsController } from './products.controller'
 import { ProductsService } from './products.service'
 
 @Module({
   providers: [ProductsService],
-  controllers: [ProductsController]
+  controllers: [ProductsController],
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])
+  ]
 })
 export class ProductsModule {}
